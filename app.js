e6f3b3880cd0fb71380021ee43fb767d88505c1e
Rahul Rajan <rahul.rajanus@gmail.com>
2020-04-25 17:41:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,12 +39,13 @@ async function makeCMSRequest(query, variables = {}, token = "") {
 
 async function makeRequest(message, clientSchemaJson, adminkey) {
     ret = failureJson()
+    encodedKey = Buffer.from(adminkey).toString("base64")
     const res = await fetch(process.env.BUZZER_URL, {
         method: 'POST',
         headers: {
             'Content-Type': `application/json`,
             'Accept'      : `application/json`,
-            'Authorization': 'Basic ' + adminkey
+            'Authorization': 'Basic ' + encodedKey
         },
         body: JSON.stringify({
             query: queryMessage,
